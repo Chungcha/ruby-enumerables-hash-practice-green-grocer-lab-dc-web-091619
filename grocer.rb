f232cfcb06d6c96808dbaa76ce9a2df8005dbfbf
Charlie Chung <charlieschung@gmail.com>
2019-09-06 19:32:36 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -54,5 +54,8 @@ def checkout(cart, coupons)
   consolidate_cart(cart).map do |key,value|
     total+=value[:price]
   end
+  def apply_coupons(consolidate_cart(cart), coupons).map do |key,value|
+    total+=value[:price]
+  end
  total
 end
